5595e94ee6e20d4f2760ae55d7010ab18a7657ed
Zhixin <liuzhixin.sg@gmail.com>
2022-01-02 14:35:17 +0800

    working on storing profile img in db

--- app/src/main/java/org/tensorflow/lite/examples/classification/RegisterActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/RegisterActivity.java
@@ -3,14 +3,26 @@ package org.tensorflow.lite.examples.classification;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.canhub.cropper.CropImage;
+import com.canhub.cropper.CropImageView;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -19,12 +31,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText et_register_username, et_register_email, et_register_password , et_confirm_password;
     Button btnRegister;
     TextView tv_to_login;
+    CircleImageView civProfile;
 
     AsyncHttpClient asyncHttpClient;
     RequestParams requestParams;
@@ -40,11 +54,51 @@ public class RegisterActivity extends AppCompatActivity {
         et_register_email = findViewById(R.id.et_register_email);
         et_register_password = findViewById(R.id.et_register_password);
         et_confirm_password = findViewById(R.id.et_confirm_password);
+        civProfile = findViewById(R.id.profile_img);
         btnRegister = findViewById(R.id.btn_register);
         tv_to_login = findViewById(R.id.tv_to_login);
 
         asyncHttpClient = new AsyncHttpClient();
         requestParams = new RequestParams();
+
+        civProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withContext(RegisterActivity.this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(RegisterActivity.this);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        if(permissionDeniedResponse.isPermanentlyDenied()){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            builder.setTitle("Permission Required");
+                            builder.setMessage("Permission to access your device storage is required to select profile image, Please enable permission to access storage in settings");
+                            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Intent intent = new Intent();
+                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.setData(Uri.fromParts("package", getPackageName(),null));
+                                    startActivityForResult(intent, 51);
+                                }
+                            });
+                            builder.setNegativeButton("Cancel",null);
+                            builder.show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                        .check();
+            }
+        });
+
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,5 +162,19 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUriContent();
+                civProfile.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
