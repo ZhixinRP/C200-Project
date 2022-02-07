2d462ac1d7ec625bab4a6627f2a0fccf0438aea5
Amos Ng <fatmosaurus@gmail.com>
2022-02-07 17:27:27 +0800

    Implemented Dummy charts for testing

--- app/src/main/java/org/tensorflow/lite/examples/classification/ChartFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/ChartFragment.java
@@ -1,5 +1,6 @@
 package org.tensorflow.lite.examples.classification;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,59 +9,79 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class ChartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    BarChart barChart;
+    PieChart pieChart;
 
     public ChartFragment() {
         // Required empty public constructor
-
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChartFragment newInstance(String param1, String param2) {
-        ChartFragment fragment = new ChartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_chart, container, false);
+        barChart = v.findViewById(R.id.tv_username);
+        pieChart = v.findViewById(R.id.equipmentBtn);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
+        for (int i = 1; i < 10; i++) {
+            float value = (float) (i * 10.0);
+
+            //Initialise chart entry
+            BarEntry barEntry = new BarEntry(i,value);
+            PieEntry pieEntry = new PieEntry(i,value);
+
+            //Add values in array list
+            barEntries.add(barEntry);
+            pieEntries.add(pieEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Employees");
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Students");
+
+        //Set colours
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        //Hide draw value
+        barDataSet.setDrawValues(false);
+        pieDataSet.setDrawValues(false);
+
+        //Set bar data
+        barChart.setData(new BarData(barDataSet));
+        pieChart.setData(new PieData(pieDataSet));
+
+        //Set animation
+        barChart.animateY(5000);
+        pieChart.animateXY(5000, 5000);
+
+        //Set description text and color
+        barChart.getDescription().setText("Employees Chart");
+        barChart.getDescription().setTextColor(Color.BLUE);
+
+        pieChart.getDescription().setText("Students Chart");
+        pieChart.getDescription().setTextColor(Color.RED);
         return v;
+
     }
 }
